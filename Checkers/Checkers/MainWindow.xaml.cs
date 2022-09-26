33a70b09fc7fa43684453d902d62045839351c6c
Sid0r0vich <dmitriysidorcool@gmail.com>
2022-09-26 23:32:00 +0300

    commit4

--- Checkers/Checkers/MainWindow.xaml.cs
+++ Checkers/Checkers/MainWindow.xaml.cs
@@ -1,27 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
+
 
 namespace Checkers
 {
+    public class Field
+    {
+        public Button button = new Button();
+        public Image image = new Image();
+
+        public MainWindow owner;
+        public int[] coords;
+
+        public Field(MainWindow owner, int row, int column)
+        {
+            this.owner = owner;
+            this.coords = new int[2] { row, column };
+
+            this.button.Background = new SolidColorBrush(Color.FromArgb(255, 169, 75, 0));
+            this.image.Margin = new Thickness(7, 7, 7, 7);
+
+            this.button.Content = this.image;
+            this.button.Click += owner.Select_Field;
+            owner.board_grid.Children.Add(this.button);
+
+            Grid.SetRow(this.button, row);
+            Grid.SetColumn(this.button, column);
+        }
+
+        public void Set_State()
+        {
+            this.image.Source = owner.board[coords[0], coords[1]] switch
+            {
+                1 => MainWindow.white_checker,
+                -1 => MainWindow.black_checker,
+                0 => MainWindow.empty,
+                _ => null,
+            };
+        }
+
+        public void Make_Move()
+        {
+
+        }
+    }
+
+    public class Game
+    {
+        public MainWindow owner;
+        public Field[,] fields = new Field[8, 8];
+
+        public Game(MainWindow owner)
+        {
+            this.owner = owner;
+        }
+
+        public void Create_Board()
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Field field = new Field(this.owner, i, j);
+                        fields[i, j] = field;
+                    }
+                }
+            }
+        }
+
+        public void Set_Position()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        fields[i, j].Set_State();
+                    }
+                }
+            }
+        }
+    }
+
     public partial class MainWindow : Window
     {
         public string PlayerName;
 
-        static string path = "C:\\Users\\Overlord\\git\\checkers\\Checkers\\Images\\";
+        public static string path = "C:\\Users\\Overlord\\git\\checkers\\Checkers\\Images\\";
         public static BitmapImage white_checker = new BitmapImage(new Uri($"{path}white_checker.png"));
         public static BitmapImage black_checker = new BitmapImage(new Uri($"{path}black_checker.png"));
         public static BitmapImage empty = new BitmapImage(new Uri($"{path}empty.png"));
@@ -38,16 +111,25 @@ namespace Checkers
             { 0,-1, 0,-1, 0,-1, 0,-1 },
         };
 
-        public System.Windows.Controls.Button[,] buttons = new System.Windows.Controls.Button[8, 8];
-
         public Button selected_field;
         public short[] selected_coords = new short[2];
+
+        public short turn = 1;
+
+        public Game game;
 
         public MainWindow()
         {
             InitializeComponent();
-            Create_Board();
+            Init();
         }
+
+        public void Init()
+        {
+            game = new Game(this);
+            game.Create_Board();
+        }
+
         public void Get_Name(object sender, RoutedEventArgs e)
         {
             PlayerName WindowName = new PlayerName();
@@ -56,69 +138,28 @@ namespace Checkers
             {
                 if (WindowName.Name != "")
                 {
-                    GridInfo.Children.Remove(ButtonPlay);
-                    MessageBox.Show("Play!");
                     this.PlayerName = WindowName.Name;
 
-                    TextBlock TextName = new TextBlock();
+                    TextBlock TextName = new TextBlock()
+                    {
+                        Height = 50,
+                        Text = $"{this.PlayerName}",
+                        FontSize = 30,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Margin = new Thickness(0, 20, 0, 0)
+                    };
 
-                    TextName.Height = 50;
-                    TextName.Text = $"{this.PlayerName}";
-                    TextName.FontSize = 30;
-                    TextName.HorizontalAlignment = HorizontalAlignment.Center;
-                    TextName.VerticalAlignment = VerticalAlignment.Top;
-                    TextName.Margin = new Thickness(0, 20, 0, 0);
+                    GridInfo.Children.Remove(ButtonPlay);
                     GridInfo.Children.Add(TextName);
 
-                    Set_Position();
+                    game.Set_Position();
                 }
                 else
                     MessageBox.Show("Please, enter your name!");
             }
         }
-        public void Create_Board()
-        {
 
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    if ((i + j) % 2 == 0)
-                    {
-                        Button button = new Button();
-                        button.Background = new SolidColorBrush(Color.FromArgb(255, 169, 75, 0));
-                        button.Click += this.Select_Field;
-                        board_grid.Children.Add(button);
-
-                        Grid.SetRow(button, i);
-                        Grid.SetColumn(button, j);
-
-                        buttons[i, j] = button;
-                    }
-                }
-            }
-        }
-        public void Set_Position()
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    if ((i + j) % 2 == 0)
-                    {
-                        System.Windows.Controls.Image image = new System.Windows.Controls.Image();
-                        image.Margin = new Thickness(7, 7, 7, 7);
-                        buttons[i,j].Content = image;
-
-                        if (board[i, j] == 1)
-                            image.Source = MainWindow.white_checker;
-                        else if (board[i, j] == -1)
-                            image.Source = MainWindow.black_checker;
-                        else image.Source = MainWindow.empty;
-                    }
-                }
-            }
-        }
         public void Select_Field(object sender, RoutedEventArgs e)
         {
             bool is_empty = true;
@@ -149,24 +190,25 @@ namespace Checkers
                 selected_field = null;
             }
         }
+
         public void Make_Move(short[] selected_coords, short row, short column)
         {
             short color = this.board[selected_coords[0], selected_coords[1]];
 
-            if (Math.Abs(column - selected_coords[1]) == 1 && row - selected_coords[0] == color)
-            {
-                this.board[row, column] = color;
-                this.board[selected_coords[0], selected_coords[1]] = 0;
-                Set_Position();
-            }
+            if (color != this.turn) return;
+
+            if (Math.Abs(column - selected_coords[1]) == 1 && row - selected_coords[0] == color) ;
             else if (Math.Abs(column - selected_coords[1]) == 2 && Math.Abs(row - selected_coords[0]) == 2 &&
                 this.board[(row + selected_coords[0]) / 2, (column + selected_coords[1]) / 2] == -color)
             {
-                this.board[row, column] = color;
-                this.board[selected_coords[0], selected_coords[1]] = 0;
                 this.board[(row + selected_coords[0]) / 2, (column + selected_coords[1]) / 2] = 0;
-                Set_Position();
             }
+            else return;
+
+            this.turn *= -1;
+            this.board[row, column] = color;
+            this.board[selected_coords[0], selected_coords[1]] = 0;
+            game.Set_Position();
         }
     }
 }
